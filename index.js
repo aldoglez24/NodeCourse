2bd71c463347fbc7baa39615fa27410e9d91e229
Aldo González <aldoalejandrog@gmail.com>
2018-08-04 23:01:37 -0500

    basic

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-var express = require('express');// Dependencia para express
-var app = express(); //Dependendcia
+/*
+Tutorial ExpressJs
+https://www.tutorialspoint.com/expressjs/
+Recomendable:
+    Instalar nodemon e iniciar con nodemon index
+*/
 
-app.get('/', function(req, res){
-   res.send("Hello world!");
+var express = require('express');// Dependencia para express
+var app = express(); //Nombre de la variable que contiene la dependencia
+
+/*Función cuando el servidor escucha algo en la dirección "/" parametros de entrada "req" y respuesta "res"
+    object(req) contiene la información de entrada como html
+*/
+
+app.get('/', function(req, res){ 
+   res.send("Hello world!"); //Es el método que regresará la respuesta con el objeto res, en este caso "Hello world!"
 });
 
-app.listen(3000);
+//Método para escuchar las peticiones app.listen(port, [host], [backlog], [callback]])
+app.listen(9090);//http://localhost:9090
